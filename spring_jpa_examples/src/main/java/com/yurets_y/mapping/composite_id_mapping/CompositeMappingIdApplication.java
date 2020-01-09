0b36c6d1188a672081bb87bbeadcf14e71bd4855
Yuriy work-pc <transpmailbox@gmail.com>
2020-01-09 16:17:00 +0200

    Add and test compositeId mapping

--- spring_jpa_examples/src/main/java/com/yurets_y/mapping/composite_id_mapping/CompositeMappingIdApplication.java
+++ spring_jpa_examples/src/main/java/com/yurets_y/mapping/composite_id_mapping/CompositeMappingIdApplication.java
@@ -1,11 +1,14 @@
 package com.yurets_y.mapping.composite_id_mapping;
 
 import com.yurets_y.mapping.composite_id_mapping.entity.Account;
+import com.yurets_y.mapping.composite_id_mapping.entity.AccountId;
 import com.yurets_y.mapping.composite_id_mapping.repo.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Optional;
 
 @SpringBootApplication
 public class CompositeMappingIdApplication implements CommandLineRunner {
@@ -25,6 +28,18 @@ public class CompositeMappingIdApplication implements CommandLineRunner {
         account.setUserName("TestUser");
         account.setUserEmail("test@user.com");
         accountRepository.save(account);
+
+        AccountId id = new AccountId("22345","USER");
+
+        Account AccountFromRepo = accountRepository.getOne(id);
+        Iterable<Account> accounts = accountRepository.findAll();
+
+
+        Optional<Account> fromRepo = accountRepository.findById(id);
+
+
+        System.out.println("Account from repo:");
+        System.out.println(fromRepo.get().getUserEmail());
 
 
 
